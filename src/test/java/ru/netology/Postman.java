61ea10238ea8385ddf14743bbf3b4129bc2fdde2
Дина Маскайкина <maskaykinadina@gmail.com>
2022-06-14 22:52:14 +0300

    отредактирован

--- src/test/java/ru/netology/Postman.java
+++ src/test/java/ru/netology/Postman.java
@@ -20,7 +20,7 @@ public class Postman {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 
